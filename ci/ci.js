aa64474c942a5df28526e49768e9f3fa7b2862ef
nicekingwei <nicekingwei@foxmail.com>
2018-11-17 18:09:16 +0800

    test bot

--- ci/ci.js
+++ ci/ci.js
@@ -84,7 +84,7 @@ function server(req, res) {
           };
           var postData = {
             'msgtype': 'text',
-            'text': {'content': committer + ':' + msg}
+            'text': {'content': committer + ': ' + msg}
           };
           var req = https.request(options);
           req.write(JSON.stringify(postData));
