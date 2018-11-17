284ea7d380c0dfbcb8b89a9b82cab69c128ec564
nicekingwei <nicekingwei@foxmail.com>
2018-11-17 15:28:37 +0800

    test bot

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 scp ci/ci.js ubuntu@zju-lambda.tech:/home/ubuntu
 scp code-runner/code-runner.js ubuntu@zju-lambda.tech:/home/ubuntu
+ssh ubuntu@zju-lambda.tech forever restart 0
+ssh ubuntu@zju-lambda.tech forever restart 1
