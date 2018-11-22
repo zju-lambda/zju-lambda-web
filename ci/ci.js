8c2dedbc3caee7e98de3336e613238fc3357093b
nicekingwei <nicekingwei@foxmail.com>
2018-11-22 20:40:05 +0800

    codb

--- ci/ci.js
+++ ci/ci.js
@@ -84,7 +84,60 @@ function server(req, res) {
           };
           var postData = {
             'msgtype': 'text',
-            'text': {'content': committer + ': ' + msg}
+            'text': {
+              'content': data.repository.name + ' ' + committer + ': ' + msg
+            }
+          };
+          var req = https.request(options);
+          req.write(JSON.stringify(postData));
+          req.end();
+        });
+      }
+      else if (
+          data.repository && data.repository.name &&
+          data.repository.name == 'codb') {
+        if (s.length == 2 && s[0] == 'branch') {
+          branch = ['cd codb', 'git checkout ' + s[1], 'cd ../'];
+        }
+        var committer = 'who';
+        if (data && data.head_commit && data.head_commit.committer) {
+          committer = data.head_commit.committer.name;
+        }
+        console.log(data);
+        var cmds =
+            [
+              'rm -rf engine codb',
+              'git clone git@git.dev.tencent.com:Nicekingwei/codb.git'
+            ].concat(branch)
+                .concat([
+                  'git clone git@code.aliyun.com:nicekingwei/engine.git',
+                  'rm -rf engine/engine_race/*.h',
+                  'rm -rf engine/engine_race/*.cc',
+                  'cp -r codb/engine_race/include/* engine/engine_race',
+                  'cp -r codb/engine_race/src/* engine/engine_race',
+                  'cd engine', 'cp -r include/ engine_race/', 'git add .',
+                  'git commit -m "' + msg + '"', 'git push', 'cd ../',
+                  'rm -rf engine codb'
+                ]);
+        var cmd = cmds.join(' && ');
+        console.log(cmd);
+        cp.exec(cmd, function(err) {
+          if (err) {
+            console.log(err);
+          }
+          var options = {
+            hostname: 'oapi.dingtalk.com',
+            port: 443,
+            path:
+                '/robot/send?access_token=150fad29d24a78689a26610b3c824c2c30e8fff8aa9677a8d9a77bcf8b3f92d4',
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+          };
+          var postData = {
+            'msgtype': 'text',
+            'text': {
+              'content': data.repository.name + ' ' + committer + ': ' + msg
+            }
           };
           var req = https.request(options);
           req.write(JSON.stringify(postData));
