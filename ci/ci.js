8905b396712443e4b3cf4039f755b95b7b73e34a
nicekingwei <nicekingwei@foxmail.com>
2018-12-08 00:49:42 +0800

    dualdb

--- ci/ci.js
+++ ci/ci.js
@@ -17,7 +17,7 @@ function server(req, res) {
         msg = data.head_commit.message.trim();
       }
       var branch = [];
-      var s = msg.split(' ')
+      var s = msg.split(' ');
       if (data.repository && data.repository.name &&
           data.repository.name == 'arcdb') {
         if (s.length == 2 && s[0] == 'branch') {
@@ -41,11 +41,10 @@ function server(req, res) {
         var cmd = cmds.join(' && ');
         console.log(cmd);
         cp.exec(cmd, console.log);
-      }
-      else if (
+      } else if (
           data.repository && data.repository.name &&
           data.repository.name == 'bootdb') {
-        if (s.length == 2 && s[0] == 'branch') {
+        if (s.length >= 2 && s[0] == 'branch') {
           branch = ['cd bootdb', 'git checkout ' + s[1], 'cd ../'];
         }
         var committer = 'who';
@@ -92,11 +91,60 @@ function server(req, res) {
           req.write(JSON.stringify(postData));
           req.end();
         });
-      }
-      else if (
+      } else if (
+          data.repository && data.repository.name &&
+          data.repository.name == 'dualdb') {
+        if (s.length >= 2 && s[0] == 'branch') {
+          branch = ['cd dualdb', 'git checkout ' + s[1], 'cd ../'];
+        }
+        var committer = 'who';
+        if (data && data.head_commit && data.head_commit.committer) {
+          committer = data.head_commit.committer.name;
+        }
+        console.log(data);
+        var cmds =
+            [
+              'rm -rf engine dualdb',
+              'git clone git@git.dev.tencent.com:Nicekingwei/dualdb.git'
+            ].concat(branch)
+                .concat([
+                  'git clone git@code.aliyun.com:nicekingwei/engine.git',
+                  'rm -rf engine/engine_race/*.h',
+                  'rm -rf engine/engine_race/*.cc',
+                  'cp -r dualdb/engine_race/include/* engine/engine_race',
+                  'cp -r dualdb/engine_race/src/* engine/engine_race',
+                  'cd engine', 'cp -r include/ engine_race/', 'git add .',
+                  'git commit -m "' + msg + '"', 'git push', 'cd ../',
+                  'rm -rf engine dualdb'
+                ]);
+        var cmd = cmds.join(' && ');
+        console.log(cmd);
+        cp.exec(cmd, function(err) {
+          if (err) {
+            console.log(err);
+          }
+          var options = {
+            hostname: 'oapi.dingtalk.com',
+            port: 443,
+            path:
+                '/robot/send?access_token=150fad29d24a78689a26610b3c824c2c30e8fff8aa9677a8d9a77bcf8b3f92d4',
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+          };
+          var postData = {
+            'msgtype': 'text',
+            'text': {
+              'content': data.repository.name + ' ' + committer + ': ' + msg
+            }
+          };
+          var req = https.request(options);
+          req.write(JSON.stringify(postData));
+          req.end();
+        });
+      } else if (
           data.repository && data.repository.name &&
           data.repository.name == 'codb') {
-        if (s.length == 2 && s[0] == 'branch') {
+        if (s.length >= 2 && s[0] == 'branch') {
           branch = ['cd codb', 'git checkout ' + s[1], 'cd ../'];
         }
         var committer = 'who';
@@ -144,6 +192,8 @@ function server(req, res) {
           req.end();
         });
       }
+
+
       res.writeHead(200);
       res.end();
     });
